580cfe34b4960da3f50dc452813abc292fd48939
Jayakrishnan U <jayakrishnanu2006@gmail.com>
2025-07-01 20:34:17 +0530

    tailwind set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
-function App() {
+    function App() {
   return (
-    <div className="App">
-     <h1>hi</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind in React!</h1>
     </div>
   );
 }
-
 export default App;
+
+ 
