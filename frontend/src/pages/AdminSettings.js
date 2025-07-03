15b6cea4d752d41a8c3fa938f99f594b964e5421
Jayakrishnan U <jayakrishnanu2006@gmail.com>
2025-07-03 22:52:23 +0530

    logout set up

--- frontend/src/pages/AdminSettings.js
+++ frontend/src/pages/AdminSettings.js
@@ -9,16 +9,25 @@ function AdminSettings() {
         navigate(`/admin/changepassword/${id}`)
             
     }
+    const handlelogout=()=>{
+        sessionStorage.clear()
+        navigate('/admin/login')
+    }
   return (
      <div className="mt-5 p-6 bg-white w-1/2 mx-auto rounded-xl shadow-md max-w-md">
   
 
-  <div onClick={()=>{navigate('/admin/settings') 
+  <div onClick={()=>{
                     handlechangepassword(id)
   }}
   className="mt-5 bg-blue-100 cursor-pointer border border-blue-300 rounded-lg p-4">
     <p className="text-lg font-semibold text-blue-700">Change Password</p>
   </div>
+  <div onClick={handlelogout}
+  className="mt-5 bg-blue-100 cursor-pointer border border-blue-300 rounded-lg p-4">
+    <p className="text-lg font-semibold text-blue-700">Logout</p>
+  </div>
+ 
  
 </div>
   )
