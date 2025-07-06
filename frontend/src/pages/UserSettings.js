20eb04fd11bfe3523bc2e46d7305a44c806408aa
Jayakrishnan U <jayakrishnanu2006@gmail.com>
2025-07-06 10:24:15 +0530

    user logout set up

--- frontend/src/pages/UserSettings.js
+++ frontend/src/pages/UserSettings.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 function UserSettings() {
     const navigate=useNavigate()
+    const handlelogout = () => {
+        sessionStorage.clear()
+        navigate('/users/login')
+    }
   return (
     <div className="mt-5 p-6 bg-white w-1/2 mx-auto rounded-xl shadow-md max-w-md">
       <div onClick={() => { navigate('/users/changepassword') }}
         className="mt-5 bg-blue-100 cursor-pointer border border-blue-300 rounded-lg p-4">
         <p className="text-lg font-semibold text-blue-700">Change Password</p>
       </div>
-      <div onClick={() => { navigate('/admin/addfooditem') }}
+      <div onClick={handlelogout}
         className="mt-5 bg-blue-100 cursor-pointer border border-blue-300 rounded-lg p-4">
         <p className="text-lg font-semibold text-blue-700">Logout</p>
       </div>
